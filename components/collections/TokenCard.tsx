c229588e737b3a463d9fee1ac8e17ea71083d983
TomCat <midoelmelegy@gmail.com>
2023-11-23 13:58:50 +0800

    Update TokenCard.tsx

--- components/collections/TokenCard.tsx
+++ components/collections/TokenCard.tsx
@@ -1,5 +1,6 @@
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useMediaQuery } from 'react-responsive'
 import {
   extractMediaType,
   TokenMedia,
