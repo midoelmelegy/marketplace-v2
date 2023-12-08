657b2cf53beb317c7c6dd61c93f41fcb8b820442
TomCat <midoelmelegy@gmail.com>
2023-12-08 12:06:24 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,7 @@ class MyDocument extends Document {
 
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
         {/* Meta tags */}
         <meta name="keywords" content="nft, ethereum, protocol" />
